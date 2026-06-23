e499926ce18c4ff8094e675b1e06b66d1c1d8a72
MarcosVerso <kgmc122@gmail.com>
2026-06-23 17:55:04 -0400

    c

--- components/BaseComponent.js
+++ components/BaseComponent.js
@@ -6,7 +6,7 @@ export class BaseComponent extends HTMLElement{
 
     connectedCallback() {
         const folderName = this.constructor._folderName;
-        const cssPath = `${folderName}/style.css`;
+        const cssPath = `../components/${folderName}/style.css`;
 
 
         console.log(cssPath);

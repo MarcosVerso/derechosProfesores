3d05b78fe5bb8b4b95b3cd78772cfed3df911bf2
MarcosVerso <kgmc122@gmail.com>
2026-06-23 16:51:07 -0400

    c

--- components/BaseComponent.js
+++ components/BaseComponent.js
@@ -5,11 +5,14 @@ export class BaseComponent extends HTMLElement{
     }
 
     connectedCallback() {
-        const folderName = this.constructor._folderName;
-        const cssPath = `https://marcosverso.github.io/derechosProfesores/components/${folderName}/style.css`;
+        const origin = window.location.origin;
+    const pathname = window.location.pathname;
 
+    // Aseguramos que el pathname termine en barra si no lo hace, y limpiamos dobles barras
+    const basePath = pathname.endsWith('/') ? pathname : `${pathname}/`;
+    const cssPath = `${origin}${basePath}components/${folderName}/style.css`;
 
-        console.log(cssPath);
+    console.log("Cargando CSS desde:", cssPath);
         this.shadowRoot.innerHTML = `
             <style>
                 @import url("${cssPath}");
